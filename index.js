1d556ec56cab28786363a40b3bef9a64cda7b8eb
sheetal1409 <103014900+sheetal1409@users.noreply.github.com>
2022-08-11 17:20:00 -0700

    Added  validation for data entered

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 let inputNum=document.getElementById("input")
 let lengthRes=document.getElementById("len-res")
 let volumeRes=document.getElementById("vol-res")
 let massRes=document.getElementById("mas-res")
+
 
 const meterToFeet =  3.281
 const feettometer=0.3048
@@ -20,7 +15,13 @@ let convertBtn=document.getElementById("convert-btn")
 
 convertBtn.addEventListener("click",function(){
    let num=inputNum.value;
+   if(num>1){
    lengthRes.textContent=`${num} meter = ${(num * meterToFeet).toFixed(3)} feet | ${num} feet = ${(num * feettometer).toFixed(3)} meter`
  volumeRes.textContent=`${num} liter = ${(num * literToGallon).toFixed(3)} gallons | ${num} gallons = ${(num * gallonToLiter).toFixed(3)} liter`
    massRes.textContent=`${num} kilo = ${(num * kiloToPound).toFixed(3)} pound | ${num} pound = ${(num * poundToKilo).toFixed(3)} kilo`
+   }
+   else
+   {
+     alert(`Enter valid data`)
+   }
 })
