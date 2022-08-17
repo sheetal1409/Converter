2fd19e61016c1c199391b49e240398c074155791
sheetal1409 <103014900+sheetal1409@users.noreply.github.com>
2022-08-17 08:38:10 -0700

    Updated validation

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let convertBtn=document.getElementById("convert-btn")
 
 convertBtn.addEventListener("click",function(){
    let num=inputNum.value;
-   if(num>1){
+   if(num>=1){
    lengthRes.textContent=`${num} meter = ${(num * meterToFeet).toFixed(3)} feet | ${num} feet = ${(num * feettometer).toFixed(3)} meter`
  volumeRes.textContent=`${num} liter = ${(num * literToGallon).toFixed(3)} gallons | ${num} gallons = ${(num * gallonToLiter).toFixed(3)} liter`
    massRes.textContent=`${num} kilo = ${(num * kiloToPound).toFixed(3)} pound | ${num} pound = ${(num * poundToKilo).toFixed(3)} kilo`
